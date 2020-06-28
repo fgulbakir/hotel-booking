8660492e67851086ac59135d269d442e83019029
fgulbakir <fatmagulbakir61@gmail.com>
2020-06-29 02:10:22 +0300

    some new methods were added to controller

--- src/main/java/javadevs/booking/BookingController.java
+++ src/main/java/javadevs/booking/BookingController.java
@@ -1,10 +1,12 @@
 package javadevs.booking;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/bookings")
@@ -22,6 +24,14 @@ public class BookingController {
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<HotelBooking> getBookings() {
         return bookings;
+
+    }
+
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price) {
+        return bookings.stream()
+                .filter(bkg -> bkg.getPricePerNight() <= price)
+                .collect(Collectors.toList());
 
     }
 }
